abfe408d9031e8767b90f4ada164cb9e7ccf34c7
Sachin S Kamath <sskamath96@gmail.com>
2016-10-28 01:14:08 +0530

    PEP-8 fixes and added .gitignore

--- pyarp/pyarplib.py
+++ pyarp/pyarplib.py
@@ -1,11 +1,11 @@
 import re
 
-banner = """  _____ __     __        _____   _____  
+banner = """  _____ __     __        _____   _____
  |  __ \\\ \   / / /\    |  __ \ |  __ \  [Author : Imad Hsissou]
  | |__) |\ \_/ / /  \   | |__) || |__) | [Author email : imad.hsissou@gmail.com]
  |  ___/  \   / / /\ \  |  _  / |  ___/  [https://github.com/7bisso]
  | |       | | / ____ \ | | \ \ | |      [version 0.1.1]
- |_|       |_|/_/    \_\|_|  \_\|_|      
+ |_|       |_|/_/    \_\|_|  \_\|_|
 
 \"Usage of Pyarp for attacking targets without prior mutual consent is illegal.\"
 
@@ -37,84 +37,85 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-manual = """			   _____MAN PAGE_____
+manual = """               _____MAN PAGE_____
 
 NAME
-	pyarp  - a friendly command-line spoofing tool wrriten in python.
+    pyarp  - a friendly command-line spoofing tool wrriten in python.
 
 COMMANDS
 
-	iface - 
-			iface <interface name>
-			Manually set your network interface
-			'eth0' is set as default.
+    iface -
+            iface <interface name>
+            Manually set your network interface
+            'eth0' is set as default.
 
-	inet  - 
-			inet [option]
-			options :
-				AF_INET : IP information
-				AF_LINK : MAC information
-				Type "inet" for both.
+    inet  -
+            inet [option]
+            options :
+                AF_INET : IP information
+                AF_LINK : MAC information
+                Type "inet" for both.
 
-	getmac - 
-			getmac <host ip address>
-			Get host MAC address.
+    getmac -
+            getmac <host ip address>
+            Get host MAC address.
 
-	scan - 
-			scan <network>, eg {scan 192.168.1.0/24}
-			Map your network.
+    scan -
+            scan <network>, eg {scan 192.168.1.0/24}
+            Map your network.
 
-	spoof - 
-			spoof <target> <target>
-			Launch MITM/ARP spoofing attack.
-			press Ctrl+C to stop.
+    spoof -
+            spoof <target> <target>
+            Launch MITM/ARP spoofing attack.
+            press Ctrl+C to stop.
 
-	help  - 
-			help [command]
-			display information about a command.
+    help  -
+            help [command]
+            display information about a command.
 
-	clear - clear screen
+    clear - clear screen
 
-	exit  - close pyarp, press Ctrl+D for clean exit
-			press Ctrl+C for forced exit.
+    exit  - close pyarp, press Ctrl+D for clean exit
+            press Ctrl+C for forced exit.
 
 NOTICE
 
-	IPv6 is not supported Yet.
+    IPv6 is not supported Yet.
 """
 
-regex_net = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$"
+regex_net = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}\
+([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$"
 regex_ip = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 regex_mac = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
 
 
 def pyarp_banner():
-	return banner
+    return banner
 
 
 def pyarp_license():
-	return license
+    return license
 
 
 def pyarp_manual():
-	return manual
+    return manual
 
 
 def regex(exp, regexp):
-	if regexp == 'net':
-		check = re.compile(regex_net)
-		match = check.match(exp)
-		return match
+    if regexp == 'net':
+        check = re.compile(regex_net)
+        match = check.match(exp)
+        return match
 
-	elif regexp == 'ip':
-		check = re.compile(regex_ip)
-		match = check.match(exp)
-		return match
+    elif regexp == 'ip':
+        check = re.compile(regex_ip)
+        match = check.match(exp)
+        return match
 
-	elif regexp == 'mac':
-		check = re.compile(regex_mac)
-		match = check.match(exp)
-		return match
+    elif regexp == 'mac':
+        check = re.compile(regex_mac)
+        match = check.match(exp)
+        return match
 
-	else:
-		return False
+    else:
+        return False
